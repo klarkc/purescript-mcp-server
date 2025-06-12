87c58f62c48b851368d08fc6da76cf54b7ce6401
Tom Wells <tom@synthesis.co.za>
2025-06-12 10:44:48 +0200

    ported tests to hspec

---
+++ test/HspecMain.hs
@@ -0,0 +1,17 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Main (main) where
+
+import Test.Hspec
+import qualified Spec.JSONConversion
+import qualified Spec.BasicDerivation
+import qualified Spec.SchemaValidation
+import qualified Spec.AdvancedDerivation
+
+main :: IO ()
+main = hspec $ do
+  describe "MCP Server" $ do
+    Spec.JSONConversion.spec
+    Spec.BasicDerivation.spec
+    Spec.SchemaValidation.spec
+    Spec.AdvancedDerivation.spec
